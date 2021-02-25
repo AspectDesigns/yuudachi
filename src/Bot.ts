40a0d750136ecaad98c207feb7514df596d982ba
Lawson Geter <lawson.geter@gmail.com>
2021-02-25 04:49:38 -0500

    (edit: 3) Added TriggerEvent() on startup.

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,6 +1,12 @@
+import { Listener } from "discord-akairo";
+import { TriggerEvent } from "./Auto/TriggerEvent";
 import BotClient from "./client/BotClient";
 import { PREFIX, TOKEN } from "./Config";
 
 const client: BotClient = new BotClient({token: TOKEN, prefix: PREFIX});
+
+client.listenerHandler.on("load", (listener: Listener) => {
+    TriggerEvent("INFO", `Loaded ${listener.event}.`)
+});
 
 client.login(TOKEN);
